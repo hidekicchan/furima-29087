95680b150b500d4e570f27a9843848eec3a74e86
hidekicchan <naniwaentertainment@gmail.com>
2020-10-25 17:54:35 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :reading_family, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :reading_first, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
+    validates :email, uniqueness: true
+    validates :password, confirmation: true, length: { minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'Password Include both letters and numbers'}
 end
