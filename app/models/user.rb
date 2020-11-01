1592c0d2bda01ff65fbfb6af862f5aae3b6c610d
hidekicchan <naniwaentertainment@gmail.com>
2020-11-01 14:00:14 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,17 @@ class User < ApplicationRecord
   #   validates :nickname
   #   validates :family_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   #   validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  #   validates :reading_family,  format: { with: /\A[ァ-ヶー－]+\z/ } 
+  #   validates :reading_family,  format: { with: /\A[ァ-ヶー－]+\z/ }
   #   validates :reading_first,  format: { with: /\A[ァ-ヶー－]+\z/ }
   #   validates :birthday
   # end
-    validates :nickname, presence: true
-    validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :reading_family, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "kana Full-width katakana characters" } 
-    validates :reading_first, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "kana Full-width katakana characters"  }
-    validates :birthday, presence: true
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :reading_family, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'kana Full-width katakana characters' }
+  validates :reading_first, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'kana Full-width katakana characters'  }
+  validates :birthday, presence: true
 
-  PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-    validates :password, length: { minimum: 6}, format: { with: PASSWORD_REGEX, message: "include both letters and numbers"}
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'include both letters and numbers' }
 end
