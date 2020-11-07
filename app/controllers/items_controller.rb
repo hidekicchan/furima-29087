eba0b9d387bb90830d2c5f93fa5b6ff65e0cee0e
hidekicchan <naniwaentertainment@gmail.com>
2020-11-07 14:50:29 +0900

    商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
   belongs_to_active_hash :status
-  belongs_to_active_hash :status
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :shipping_date
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 0 } 
+    validates :status_id, numericality: { other_than: 0 } 
+    validates :shipping_fee_id, numericality: { other_than: 0 } 
+    validates :prefecture_id, numericality: { other_than: 0 } 
+    validates :shipping_date_id, numericality: { other_than: 0 } 
+    validates :price, format: {with: /\A[0-9]+\z/i },numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
 end
