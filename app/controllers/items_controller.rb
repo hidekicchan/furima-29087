d14c8598f904ba62af5821b7797cfbb74b416b46
hidekicchan <naniwaentertainment@gmail.com>
2020-11-09 15:10:18 +0900

    商品出品機能　修正#3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, :image)
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
