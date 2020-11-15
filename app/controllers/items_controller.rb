b241052ac490188ae350ce50ce36530dc4cf4ac4
hidekicchan <naniwaentertainment@gmail.com>
2020-11-15 16:24:30 +0900

    商品削除機能　修正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,6 +39,10 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     redirect_to root_path
+    if current_user.id == @item.user_id
+    else
+      redirect_to root_path
+    end
   end
 
   private
