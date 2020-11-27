b9e491d76614a400fcca8290c3be46efae7ca6fd
hidekicchan <naniwaentertainment@gmail.com>
2020-11-27 20:41:28 +0900

    購入記録登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,23 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
+   @items = Item.find(params[:item_id])
+   @order = UserOrder.new
   end
+
+  def create
+    @items = Item.find(params[:item_id])
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :tel_number, :order_id).merge(user_id: current_user.id)
+  end
+
 end
